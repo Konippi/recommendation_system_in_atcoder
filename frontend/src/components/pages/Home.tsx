915720711ac5282797c83cae1bcabdbded17cd84
Konippi <konippi-114@outlook.jp>
2022-11-14 14:54:47 +0900

    added indicator

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,19 +1,20 @@
-import React, {FC, ReactElement} from "react";
+import React, {FC} from "react";
 import {RecommendedProblemList, SubmissionsTable} from "../mui";
 import {useApiData} from "../../hooks";
 import {ApiResponseType} from "../../types/ApiResponseType";
+import {Box, CircularProgress} from "@mui/material";
 
 type Props = {
-    userName: string;
+    userName?: string;
 };
 
 const Home: FC<Props> = ({userName}: Props) => {
-    const data: ApiResponseType | undefined = useApiData(userName);
+    const data: ApiResponseType | undefined = useApiData(userName!);
 
     return (
         <>
-            {data &&
-                <>
+            {data ?
+                <div>
                     <div className={"list"}>
                         <h2>Recommended Problems</h2>
                         <RecommendedProblemList recommendedProblems={data.api_response.recommended_problems} />
@@ -23,7 +24,10 @@ const Home: FC<Props> = ({userName}: Props) => {
                         <h2>Your Submissions</h2>
                         <SubmissionsTable submissionInfo={data.api_response.submission_info} />
                     </div>
-                </>
+                </div>:
+                <Box sx={{display: "flex", justifyContent: "center", marginTop: "20%"}}>
+                    <CircularProgress />
+                </Box>
             }
         </>
     )
